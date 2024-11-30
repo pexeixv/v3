e8393cc3869eb4b02611b4c02ebe0798f87fa96b
pexeixv <gavineditz@gmail.com>
2024-11-30 23:36:10 +0530

    Install Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()]
+});
