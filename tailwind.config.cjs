9e5209f379997d032a8f9d20f5cde61a84627361
pexeixv <gavineditz@gmail.com>
2024-12-01 10:25:58 +0530

    Add color scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,21 @@ module.exports = {
       fontFamily: {
         outfit: ["'Outfit Variable'", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        mint: {
+          50: "#EDFDFB",
+          100: "#DFFBF9",
+          200: "#BFF7F3",
+          300: "#9BF3EB",
+          400: "#7BEFE5",
+          500: "#5CEBDF",
+          600: "#20E4D3",
+          700: "#15ADA0",
+          800: "#0F766D",
+          900: "#073B37",
+          950: "#020B0D",
+        },
+      },
     },
   },
   plugins: [],
